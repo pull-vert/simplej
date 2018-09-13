5508fc92e02a934c414a2ef18c5bdfe4d84c05ac
frederic.montariol <frederic.montariol@adventiel.fr>
2018-09-13 15:57:38 +0200

    second commit

--- simplej-stdlib-jdk8/src/main/java/org/simplej/annotations/NotNull.java
+++ simplej-stdlib-jdk8/src/main/java/org/simplej/annotations/NotNull.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2018 SimpleJ's author : Frédéric Montariol. Use of this source code is governed by the Apache 2.0 license.
+ */
+
 package org.simplej.annotations;
 
 import javax.annotation.Nonnull;
